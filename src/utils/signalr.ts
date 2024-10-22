3ac92fea19a354b5323d93dcd815286024d6643b
xiaochanghai <xiaochanghai@foxmail.com>
2024-10-22 15:49:16 +0800

    feat: 🚀 modify signalr

--- src/utils/signalr.ts
+++ src/utils/signalr.ts
@@ -1,5 +1,10 @@
 import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
-const signalr = new HubConnectionBuilder().withUrl("/api/api2/chatHub").configureLogging(LogLevel.None).build();
+let baseURL = import.meta.env.VITE_API_URL as string;
+let VITE_USER_NODE_ENV = import.meta.env.VITE_USER_NODE_ENV as string;
+const signalr = new HubConnectionBuilder()
+  .withUrl((VITE_USER_NODE_ENV == "development" ? baseURL : "") + "/chat")
+  .configureLogging(LogLevel.Information)
+  .build();
 
 export default signalr;
