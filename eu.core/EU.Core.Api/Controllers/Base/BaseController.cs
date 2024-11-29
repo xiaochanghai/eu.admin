f6d5de3bb2d94de73a19f39f6f0921501eab804e
xiaochanghai <xiaochanghai@foxmail.com>
2024-11-29 16:57:38 +0800

    feat: 🚀 optimize base Controller

--- eu.core/EU.Core.Api/Controllers/Base/BaseController.cs
+++ eu.core/EU.Core.Api/Controllers/Base/BaseController.cs
@@ -11,7 +11,7 @@ namespace EU.Core.Controllers;
 /// <typeparam name="TEntityDto"></typeparam>
 /// <typeparam name="TInsertDto"></typeparam>
 /// <typeparam name="TEditDto"></typeparam>
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 public class BaseController<IServiceBase, TEntity, TEntityDto, TInsertDto, TEditDto> : Controller
 {
     #region 初始化
