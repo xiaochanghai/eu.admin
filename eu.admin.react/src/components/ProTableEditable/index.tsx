c422e1efc9ceea88e8d10b71151d4883be4d6c68
xiaochanghai <xiaochanghai@foxmail.com>
2024-12-25 23:48:57 +0800

    feat: 🚀 pro table

---
+++ eu.admin.react/src/components/ProTableEditable/index.tsx
@@ -0,0 +1,319 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "@/redux";
+import { Popconfirm, Button, Modal, Descriptions, Space, Skeleton } from "antd";
+import { EditableProTable } from "@ant-design/pro-components";
+import { Loading } from "@/components/Loading/index";
+import { getModuleLogInfo, getModuleInfo } from "@/api/modules/module";
+import { RootState, useSelector } from "@/redux";
+import { ModuleInfo, ModifyType, RecordLogData } from "@/api/interface/index";
+import { setModuleInfo } from "@/redux/modules/module";
+import http from "@/api";
+import { pagination1 } from "@/config/proTable";
+import { query } from "@/api/modules/module";
+import { message } from "@/hooks/useMessage";
+import { Icon } from "@/components/Icon";
+
+const Index: React.FC<any> = props => {
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
+  const moduleInfos = useSelector((state: RootState) => state.module.moduleInfos);
+  let { moduleCode, modifyType, masterId, tableRef, addCallBack, editCallBack, successCallBack, failCallBack } = props;
+  let moduleInfo = moduleInfos[moduleCode] as ModuleInfo;
+  let { masterColumn, url } = moduleInfo || {};
+
+  const [editableKeys, setEditableRowKeys] = useState<React.Key[]>([]);
+  const [dataSource, setDataSource] = useState<any>([]);
+  const [selectedRowKeys, setSelectedRowKeys] = React.useState([]);
+  const [recordLogVisible, setRecordLogVisible] = useState(false);
+  const [recordLogData, setRecordLogData] = useState<RecordLogData | null>(null);
+
+  useEffect(() => {
+    const getModuleInfo1 = async () => {
+      let { Data } = await getModuleInfo(moduleCode);
+      dispatch(setModuleInfo(Data));
+    };
+    if (!moduleInfo) getModuleInfo1();
+
+    setIsLoading(false);
+  }, []);
+
+  // 定义选择行的变化时的回调函数
+  // const onSelectChange = (keys: any, rows: any) => {
+  const onSelectChange = (keys: any) => {
+    setSelectedRowKeys(keys);
+    // 可以在这里处理选中行的数据，例如执行某些操作
+  };
+  const actionColumn = {
+    title: "操作",
+    dataIndex: "option",
+    fixed: "right",
+    valueType: "option",
+    width: 150,
+    render: (_text: any, record: any, _: any, action: any) => [
+      <a
+        key="editable"
+        onClick={() => {
+          if (editableKeys.length > 0) action?.saveEditable?.(editableKeys[0]);
+          action?.startEditable?.(record.ID);
+          if (editCallBack) editCallBack();
+        }}
+      >
+        编辑
+      </a>,
+      <Popconfirm
+        title="提醒"
+        description="是否确定删除记录?"
+        onConfirm={async () => {
+          let { Success, Message } = await http.delete<any>(url + "/" + record.ID);
+          if (Success) message.success(Message);
+          if (tableRef.current) tableRef.current.reload();
+        }}
+        okType="danger"
+        okText="确定"
+        cancelText="取消"
+      >
+        <a key="delete">删除</a>
+      </Popconfirm>
+    ]
+  };
+  let columns: any = [];
+  if (modifyType == ModifyType.Edit) {
+    if (moduleInfo && moduleInfo.columns) columns = [...moduleInfo.columns, actionColumn];
+  } else if (moduleInfo && moduleInfo.columns) columns = [...moduleInfo.columns];
+
+  const showLogRecord = async (selectedRows: any) => {
+    setRecordLogVisible(true);
+    let { Data } = await getModuleLogInfo({ moduleCode, id: selectedRows[0].ID });
+    setRecordLogData(Data);
+  };
+  const showLogRecordCancel = () => {
+    setRecordLogVisible(false);
+    setRecordLogData(null);
+  };
+
+  const toolBarRender = (action: any, { selectedRows, selectedRowKeys }: any) => [
+    <Space style={{ display: "flex", justifyContent: "center" }}>
+      {addCallBack ? (
+        <Button
+          disabled={modifyType == ModifyType.Edit ? false : true}
+          type="primary"
+          icon={<Icon name="PlusOutlined" />}
+          onClick={() => {
+            if (!masterId) {
+              message.error("请先保存主表数据！");
+              return;
+            }
+            if (addCallBack) addCallBack();
+          }}
+        >
+          添加
+        </Button>
+      ) : null}
+      {/* {moduleInfo && moduleInfo.Success == true && moduleInfo.actions.includes("ImportExcel") && !IsView ? (
+        <Button
+          icon={<Icon name="excel-import" />}
+          onClick={() => {
+            tableAction = action;
+            setUploadExcelVisible(true);
+          }}
+        >
+          Excel导入
+        </Button>
+      ) : null}  */}
+      {moduleInfo && moduleInfo.Success == true && moduleInfo.menuData.length > 0 ? (
+        <>
+          {moduleInfo.menuData.map((item: any) => {
+            return (
+              <Button
+                key={item.ID}
+                icon={item.Icon ? <Icon name={item.Icon} /> : null}
+                onClick={() => {
+                  props[item.FunctionCode](action, selectedRows, selectedRowKeys);
+                }}
+              >
+                {item.FunctionName}
+              </Button>
+            );
+          })}
+        </>
+      ) : null}
+    </Space>,
+    selectedRows && selectedRows.length == 1 && (
+      <Button onClick={() => showLogRecord(selectedRows)}>
+        <Icon name="UnorderedListOutlined" /> 日志
+      </Button>
+    ),
+    // selectedRows && selectedRows.length == 1 && selectedRows[0].AuditStatus == "Add" && (
+    //   <Button onClick={() => showLogRecord(selectedRows)}>审核</Button>
+    // ),
+    // selectedRows && selectedRows.length == 1 && selectedRows[0].AuditStatus == "CompleteAudit" && (
+    //   <Button danger onClick={() => showLogRecord(selectedRows)}>
+    //     撤销
+    //   </Button>
+    // ),
+
+    selectedRows && selectedRows.length > 0 && (
+      <Space style={{ display: "flex", justifyContent: "center" }}>
+        {moduleInfo && moduleInfo.Success == true && moduleInfo.hideMenu.length > 0 ? (
+          <>
+            {moduleInfo.hideMenu.map((item: any) => {
+              return (
+                <Button
+                  key={item.ID}
+                  onClick={() => {
+                    props[item.FunctionCode](action, selectedRows, selectedRowKeys);
+                  }}
+                >
+                  {item.Icon ? <Icon name={item.Icon} /> : null}
+                  {item.FunctionName}
+                </Button>
+              );
+            })}
+          </>
+        ) : null}
+      </Space>
+    )
+  ];
+
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {moduleInfo && columns ? (
+            <>
+              <EditableProTable
+                rowKey="ID"
+                className="ant-pro-table-scroll"
+                // scroll={{ x: 1000, y: "100%" }}
+                scroll={{ scrollToFirstRowOnChange: true, x: columns.length * 100, y: "100%" }}
+                recordCreatorProps={false}
+                rowSelection={{
+                  alwaysShowAlert: true,
+                  onChange: onSelectChange,
+                  selectedRowKeys
+                  // onSelect, //用户手动选择/取消选择某行的回调
+                  //   onSelectMultiple: onMulSelect, //用户使用键盘 shift 选择多行的回调
+                  //   onSelectAll: onMulSelect, //用户手动选择/取消选择所有行的回调
+                }}
+                actionRef={tableRef}
+                // tableAlertRender={({ selectedRowKeys, selectedRows, onCleanSelected }) => {
+
+                // toolBarRender={() => [
+                //   <Space size={0}>
+                //     <Button
+                //       disabled={modifyType == ModifyType.Edit ? false : true}
+                //       onClick={() => {
+                //         if (!id) {
+                //           message.error("请先保存主表数据！");
+                //           return;
+                //         }
+                //         setMaterialQueryVisible(true);
+                //       }}
+                //     >
+                //       添加
+                //     </Button>
+                //   </Space>
+                // ]}
+                // toolBarRender={() => [
+                //   <Space style={{ display: "flex", justifyContent: "center" }}>
+                //     {addCallBack ? (
+                //       <Button
+                //         disabled={modifyType == ModifyType.Edit ? false : true}
+                //         type="primary"
+                //         icon={<Icon name="PlusOutlined" />}
+                //         onClick={() => {
+                //           if (!masterId) {
+                //             message.error("请先保存主表数据！");
+                //             return;
+                //           }
+                //           if (addCallBack) addCallBack();
+                //         }}
+                //       >
+                //         添加
+                //       </Button>
+                //     ) : null}
+                //   </Space>
+                // ]}
+                toolBarRender={toolBarRender}
+                tableAlertRender={false}
+                tableAlertOptionRender={false}
+                columns={columns}
+                request={async (_params, sorter, filterCondition) => {
+                  if (masterId) {
+                    if (masterColumn) filterCondition = { ...filterCondition, [masterColumn]: masterId };
+                    let result = await query({
+                      paramData: JSON.stringify(_params),
+                      sorter: JSON.stringify(sorter),
+                      filter: JSON.stringify(filterCondition),
+                      moduleCode
+                    });
+                    return {
+                      data: result.data,
+                      total: result.total,
+                      success: true
+                    };
+                  } else
+                    return {
+                      data: [],
+                      total: 0,
+                      success: true
+                    };
+                }}
+                pagination={pagination1}
+                options={{
+                  search: true,
+                  fullScreen: true,
+                  reload: true,
+                  setting: true,
+                  density: true
+                }}
+                value={dataSource}
+                onChange={setDataSource}
+                editable={{
+                  type: "multiple",
+                  editableKeys,
+                  onSave: async (rowKey, data: any, _row) => {
+                    let params = { ...data, ModuleCode: moduleCode, masterId };
+                    let { Success, Data } = await http.put<any>(url + "/UpdateReturn/" + rowKey, params);
+                    if (Success) {
+                      if (successCallBack) data = successCallBack(data, Data);
+                    } else if (failCallBack) data = failCallBack();
+                  },
+                  onChange: setEditableRowKeys
+                }}
+                {...props}
+              />
+              {moduleInfo && moduleInfo.Success == true ? (
+                <Modal title="日志" open={recordLogVisible} width={1000} footer={null} onCancel={showLogRecordCancel}>
+                  {recordLogData ? (
+                    <Descriptions title="表信息" bordered>
+                      <Descriptions.Item label="表名称">{recordLogData.TableName}</Descriptions.Item>
+                      <Descriptions.Item label="表主键" span={2}>
+                        {recordLogData.ID}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="创建人">{recordLogData.CreatedBy} </Descriptions.Item>
+                      <Descriptions.Item label="最后修改人" span={2}>
+                        {recordLogData.UpdateBy}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="创建时间">{recordLogData.CreatedTime} </Descriptions.Item>
+                      <Descriptions.Item label="最后修改时间" span={2}>
+                        {recordLogData.UpdateTime}
+                      </Descriptions.Item>
+                    </Descriptions>
+                  ) : (
+                    <Skeleton active />
+                  )}
+                </Modal>
+              ) : null}
+            </>
+          ) : null}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Index;
