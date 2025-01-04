42298c0d38ed2ca238d1a8cb70bd7da8a1a70457
xiaochanghai <xiaochanghai@foxmail.com>
2025-01-04 22:30:52 +0800

    feat: 🚀 iv  safe config

---
+++ eu.admin.react/src/views/stock/safeConfig/index.tsx
@@ -0,0 +1,8 @@
+import Main from "../../system/common/main";
+import React from "react";
+
+const Index: React.FC = () => {
+  return <Main moduleCode="IV_SAFE_CONFIG_MNG" />;
+};
+
+export default Index;
