edadf51bff59e2e9d0c595a904d56094b9d770f1
xiaochanghai <xiaochanghai@foxmail.com>
2025-02-22 00:19:45 +0800

    feat: 🚀  stock detail

---
+++ eu.admin.react/src/views/stock/detail/index.tsx
@@ -0,0 +1,8 @@
+import Main from "../../system/common/main";
+import React from "react";
+
+const Index: React.FC = () => {
+  return <Main moduleCode="IV_DETAIL_MNG" />;
+};
+
+export default Index;
