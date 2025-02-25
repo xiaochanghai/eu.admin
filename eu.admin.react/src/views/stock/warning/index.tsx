81fc86dd110dc71034587b631d02bcb00992c134
xiaochanghai <xiaochanghai@foxmail.com>
2025-02-25 17:50:18 +0800

    feat: 🚀iv warn order

--- eu.admin.react/src/views/stock/warning/index.tsx
+++ eu.admin.react/src/views/stock/warning/index.tsx
@@ -2,7 +2,7 @@ import Main from "../../system/common/main";
 import React from "react";
 
 const Index: React.FC = () => {
-  return <Main moduleCode="IV_DETAIL_MNG" />;
+  return <Main moduleCode="IV_SECURITY_WARNING_MNG" />;
 };
 
 export default Index;
