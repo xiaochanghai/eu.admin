d107d5f2c422d4042d277a652521b6a0f6933ec0
xiaochanghai <xiaochanghai@foxmail.com>
2025-02-25 16:33:37 +0800

    feat: 🚀iv chan order

---
+++ eu.admin.react/src/views/stock/change/index.tsx
@@ -0,0 +1,8 @@
+import Main from "../../system/common/main";
+import React from "react";
+
+const Index: React.FC = () => {
+  return <Main moduleCode="IV_STOCK_QTY_CHANGE_MNG" />;
+};
+
+export default Index;
