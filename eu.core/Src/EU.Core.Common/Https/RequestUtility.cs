b873795c7f0f1f7949e7b11d9dd6b12cf68a84fb
xiaochanghai <xiaochanghai@foxmail.com>
2025-05-13 11:22:07 +0800

    feat(https): 添加RequestUtility和RestSharp.Helper类以支持HTTP请求
    新增RequestUtility类用于封装HTTP请求逻辑，支持GET和POST请求，并返回ServiceResult<T>类型的结果。同时，添加RestSharp.Helper类作为底层工具，用于创建和发送HTTP请求，支持证书添加、URL设置、请求创建等功能。这些更改旨在简化HTTP请求的处理，提高代码的可维护性和可重用性。

---
+++ eu.core/Src/EU.Core.Common/Https/RequestUtility.cs
@@ -0,0 +1,65 @@
+﻿using Elasticsearch.Net;
+using EU.Core.Common.Const;
+using EU.Core.Common.Helper;
+using EU.Core.Model;
+using Magicodes.IE.Core;
+using RestSharp;
+
+namespace EU.Core.Common.Https;
+
+public class RequestUtility
+{
+    private RestSharp.Helper helper;
+
+    public RequestUtility()
+    {
+        helper = new RestSharp.Helper();
+    }
+    public async Task<RestResponse> Get(string baseUrl)
+    {
+        //helper.AddCertificate("", "");
+        var client = helper.SetUrl(baseUrl, "");
+        var request = helper.CreateGetRequest();
+        request.RequestFormat = DataFormat.Json;
+        //request.Timeout
+        var response = await helper.GetResponseAsync(client, request);
+        return response;
+    }
+    public async Task<ServiceResult<T>> Get<T>(string baseUrl)
+    {
+        var response = await Get(baseUrl);
+        if (response.IsSuccessful)
+        {
+            var content = response.Content;
+            return ServiceResult<T>.OprateSuccess(JsonHelper.JsonToObj<T>(content), ResponseText.QUERY_SUCCESS);
+        }
+        else
+            return ServiceResult<T>.OprateFailed(response.ErrorMessage);
+    }
+    public async Task<RestResponse> Post(string baseUrl, object postData)
+    {
+        //helper.AddCertificate("", "");
+        var client = helper.SetUrl(baseUrl, "");
+        var request = helper.CreateGetRequest();
+        request.RequestFormat = DataFormat.Json;
+        request.Method = Method.Post;
+        request.AddHeader("Content-Type", "application/json");
+
+        if (postData != null)
+            request.AddJsonBody(postData);
+
+        var response = await helper.GetResponseAsync(client, request);
+        return response;
+    }
+    public async Task<ServiceResult<T>> Post<T>(string baseUrl, object postData)
+    {
+        var response = await Post(baseUrl, postData);
+        if (response.IsSuccessful)
+        {
+            var content = response.Content;
+            return ServiceResult<T>.OprateSuccess(JsonHelper.JsonToObj<T>(content), ResponseText.QUERY_SUCCESS);
+        }
+        else
+            return ServiceResult<T>.OprateFailed(response.ErrorMessage);
+    }
+}
