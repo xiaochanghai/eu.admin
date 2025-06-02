b34bdbee9372557d1c8e91de23421dcdbe59c055
xiaochanghai <xiaochanghai@foxmail.com>
2025-06-02 23:20:07 +0800

    refactor(privilege/user): 移除未使用的FormPage组件并简化用户管理页面
    移除用户管理页面中未使用的FormPage组件导入和DynamicFormPage属性，简化组件结构。原FormPage.tsx文件已删除，因其功能不再需要。

--- eu.admin.react/src/views/system/privilege/user/index.tsx
+++ eu.admin.react/src/views/system/privilege/user/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import TableList from "../../common/components/TableList";
-import FormPage from "./FormPage";
 
 const Index: React.FC<any> = () => {
-  return <TableList moduleCode="SM_USER_MNG" DynamicFormPage={FormPage} />;
+  return <TableList moduleCode="SM_USER_MNG" />;
 };
 
 export default React.memo(Index);
