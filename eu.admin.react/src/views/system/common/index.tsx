5059038d2da9d307b68a67fde6e52bf50f906cab
xiaochanghai <xiaochanghai@foxmail.com>
2025-06-02 22:29:40 +0800

    refactor(模块加载): 优化模块信息获取逻辑并添加错误处理
    重构模块主页面和通用页面组件，主要改进包括：
    1. 使用useCallback优化函数性能
    2. 添加错误处理逻辑
    3. 完善类型定义和代码注释
    4. 使用React.memo提升组件性能
    5. 统一变量命名和代码风格

--- eu.admin.react/src/views/system/common/index.tsx
+++ eu.admin.react/src/views/system/common/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 import { getModuleInfo } from "@/api/modules/module";
 import { setModuleInfo } from "@/redux/modules/module";
@@ -6,29 +6,43 @@ import { RootState, useSelector, useDispatch } from "@/redux";
 import FormIndex from "./components/FormIndex";
 import { Loading } from "@/components";
 
-// const RouterGuard: React.FC<RouterGuardProps> = props => {
-const Index: React.FC = () => {
+/**
+ * 通用模块页面组件
+ *
+ * 该组件通过URL路径获取模块代码，加载对应的模块信息，
+ * 并渲染相应的表单组件。如果模块信息不存在，会先从服务器获取。
+ */
+const Index: React.FC = React.memo(() => {
   const dispatch = useDispatch();
   const { pathname } = useLocation();
-  let arr = pathname.split("/");
 
-  const moduleCode = arr[arr.length - 1];
+  // 从URL路径中提取模块代码
+  const pathSegments = pathname.split("/");
+  const moduleCode = pathSegments[pathSegments.length - 1];
+
+  // 从Redux获取模块信息
   const moduleInfos = useSelector((state: RootState) => state.module.moduleInfos);
-  let moduleInfo = moduleInfos[moduleCode];
-  const getModuleInfo1 = async () => {
-    let { Data } = await getModuleInfo(moduleCode);
-    dispatch(setModuleInfo(Data));
-  };
-  useEffect(() => {
-    if (!moduleInfo) getModuleInfo1();
-  }, []);
+  const moduleInfo = moduleInfos[moduleCode];
 
-  return (
-    <React.Fragment>
-      {/* <h1>路由参数：{moduleCode}</h1> */}
-      {moduleInfo && moduleCode ? <FormIndex moduleCode={moduleCode} /> : <Loading />}
-    </React.Fragment>
-  );
-};
+  /**
+   * 获取模块信息并存储到Redux
+   */
+  const fetchModuleInfo = useCallback(async () => {
+    try {
+      const { Data } = await getModuleInfo(moduleCode);
+      if (Data) dispatch(setModuleInfo(Data));
+    } catch (error) {
+      console.error(`获取模块[${moduleCode}]信息失败:`, error);
+      // 这里可以添加错误处理，如显示错误提示等
+    }
+  }, [moduleCode, dispatch]);
+
+  // 组件挂载或moduleCode变化时，检查并获取模块信息
+  useEffect(() => {
+    if (!moduleInfo && moduleCode) fetchModuleInfo();
+  }, [moduleInfo, moduleCode, fetchModuleInfo]);
+
+  return <React.Fragment>{moduleInfo && moduleCode ? <FormIndex moduleCode={moduleCode} /> : <Loading />}</React.Fragment>;
+});
 
 export default Index;
--- eu.admin.react/src/views/system/common/main.tsx
+++ eu.admin.react/src/views/system/common/main.tsx
@@ -1,31 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { getModuleInfo } from "@/api/modules/module";
 import { setModuleInfo } from "@/redux/modules/module";
 import { RootState, useSelector, useDispatch } from "@/redux";
 import FormIndex from "./components/FormIndex";
 import { Loading } from "@/components";
 
-// const RouterGuard: React.FC<RouterGuardProps> = props => {
-const Index: React.FC<any> = props => {
+/**
+ * 模块主页面属性接口
+ */
+interface ModuleIndexProps {
+  /** 模块代码，用于获取模块信息和渲染对应表单 */
+  moduleCode: string;
+}
+
+/**
+ * 模块主页面组件
+ *
+ * 该组件负责加载模块信息并渲染对应的表单组件。
+ * 如果模块信息不存在，会先从服务器获取，然后存储到Redux中。
+ *
+ * @param props 组件属性
+ */
+const Main: React.FC<ModuleIndexProps> = React.memo(props => {
   const dispatch = useDispatch();
   const { moduleCode } = props;
 
+  // 从Redux获取模块信息
   const moduleInfos = useSelector((state: RootState) => state.module.moduleInfos);
-  let moduleInfo = moduleInfos[moduleCode];
-  const getModuleInfo1 = async () => {
-    let { Data } = await getModuleInfo(moduleCode);
-    dispatch(setModuleInfo(Data));
-  };
+  const moduleInfo = moduleInfos[moduleCode];
+
+  /**
+   * 获取模块信息并存储到Redux
+   */
+  const fetchModuleInfo = useCallback(async () => {
+    try {
+      const { Data } = await getModuleInfo(moduleCode);
+      if (Data) dispatch(setModuleInfo(Data));
+    } catch (error) {
+      console.error("获取模块信息失败:", error);
+      // 这里可以添加错误处理，如显示错误提示等
+    }
+  }, [moduleCode, dispatch]);
+
+  // 组件挂载或moduleCode变化时，检查并获取模块信息
   useEffect(() => {
-    if (!moduleInfo) getModuleInfo1();
-  }, []);
+    if (!moduleInfo) fetchModuleInfo();
+  }, [moduleInfo, fetchModuleInfo]);
 
-  return (
-    <React.Fragment>
-      {/* <h1>路由参数：{moduleCode}</h1> */}
-      {moduleInfo && moduleCode ? <FormIndex moduleCode={moduleCode} /> : <Loading />}
-    </React.Fragment>
-  );
-};
+  return <React.Fragment>{moduleInfo && moduleCode ? <FormIndex moduleCode={moduleCode} /> : <Loading />}</React.Fragment>;
+});
 
-export default Index;
+export default Main;
