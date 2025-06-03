b36a6b20b8e00822dde2fff6e6c77f524b5eb8f8
xiaochanghai <xiaochanghai@foxmail.com>
2025-06-03 12:21:20 +0800

    refactor(ModuleLog): 重构模块日志组件并更新开发环境配置
    重构模块日志组件，优化代码结构和可读性：
    1. 使用接口定义类型，增加代码注释
    2. 使用useCallback优化事件处理函数
    3. 拆分渲染逻辑到单独函数
    4. 改进复制功能的实现

--- eu.admin.react/src/components/Common/ModuleLog.tsx
+++ eu.admin.react/src/components/Common/ModuleLog.tsx
@@ -1,60 +1,98 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Descriptions, Skeleton } from "antd";
 import { message } from "@/hooks/useMessage";
 import useClipboard from "@/hooks/useClipboard";
 import { Icon } from "@/components/Icon";
 
-type ModuleLogProps = {
-  log: ModuleLogProps1 | null;
-};
-type ModuleLogProps1 = {
+/**
+ * 模块日志数据接口
+ */
+interface ModuleLogData {
+  /** 表名称 */
   TableName: string;
+  /** 创建人 */
   CreatedBy: string;
+  /** 记录ID */
   ID: string;
+  /** 更新人 */
   UpdateBy: string;
+  /** 创建时间 */
   CreatedTime: string;
+  /** 更新时间 */
   UpdateTime: string;
-};
+}
+
+/**
+ * 模块日志组件属性接口
+ */
+interface ModuleLogProps {
+  /** 日志数据，可能为空 */
+  log: ModuleLogData | null;
+}
+
+/**
+ * 模块日志组件
+ *
+ * 用于显示数据记录的元数据信息，包括创建和修改的详细信息
+ *
+ * @param props 组件属性
+ */
 const ModuleLog: React.FC<ModuleLogProps> = ({ log }) => {
   const { copyToClipboard } = useClipboard();
-  const handleCopy = () => {
-    if (log != null) copyToClipboard(log.ID);
-    message.success("复制成功 ！");
+
+  /**
+   * 复制记录ID到剪贴板
+   */
+  const handleCopyId = useCallback(() => {
+    if (log?.ID) {
+      copyToClipboard(log.ID);
+      message.success("复制成功！");
+    }
+  }, [log, copyToClipboard]);
+
+  /**
+   * 复制表名称到剪贴板
+   */
+  const handleCopyTableName = useCallback(() => {
+    if (log?.TableName) {
+      copyToClipboard(log.TableName);
+      message.success("复制成功！");
+    }
+  }, [log, copyToClipboard]);
+
+  /**
+   * 渲染日志详情
+   */
+  const renderLogDetails = () => {
+    if (!log) return <Skeleton active />;
+
+    return (
+      <Descriptions bordered>
+        <Descriptions.Item label="表名称">
+          {log.TableName}
+          <a className="ml-5" onClick={handleCopyTableName} title="复制表名称">
+            <Icon name="CopyOutlined" />
+          </a>
+        </Descriptions.Item>
+        <Descriptions.Item label="表主键" span={2}>
+          {log.ID}
+          <a className="ml-5" onClick={handleCopyId} title="复制ID">
+            <Icon name="CopyOutlined" />
+          </a>
+        </Descriptions.Item>
+        <Descriptions.Item label="创建人">{log.CreatedBy}</Descriptions.Item>
+        <Descriptions.Item label="最后修改人" span={2}>
+          {log.UpdateBy}
+        </Descriptions.Item>
+        <Descriptions.Item label="创建时间">{log.CreatedTime}</Descriptions.Item>
+        <Descriptions.Item label="最后修改时间" span={2}>
+          {log.UpdateTime}
+        </Descriptions.Item>
+      </Descriptions>
+    );
   };
-  const handleCopy1 = () => {
-    if (log != null) copyToClipboard(log.TableName);
-    message.success("复制成功 ！");
-  };
-  return (
-    <React.Fragment>
-      {log ? (
-        <Descriptions bordered>
-          <Descriptions.Item label="表名称">
-            {log.TableName}
-            <a className="ml-5" onClick={handleCopy1}>
-              <Icon name="CopyOutlined" />
-            </a>
-          </Descriptions.Item>
-          <Descriptions.Item label="表主键" span={2}>
-            {log.ID}
-            <a className="ml-5" onClick={handleCopy}>
-              <Icon name="CopyOutlined" />
-            </a>
-          </Descriptions.Item>
-          <Descriptions.Item label="创建人">{log.CreatedBy} </Descriptions.Item>
-          <Descriptions.Item label="最后修改人" span={2}>
-            {log.UpdateBy}
-          </Descriptions.Item>
-          <Descriptions.Item label="创建时间">{log.CreatedTime} </Descriptions.Item>
-          <Descriptions.Item label="最后修改时间" span={2}>
-            {log.UpdateTime}
-          </Descriptions.Item>
-        </Descriptions>
-      ) : (
-        <Skeleton active />
-      )}
-    </React.Fragment>
-  );
+
+  return renderLogDetails();
 };
 
 export default React.memo(ModuleLog);
