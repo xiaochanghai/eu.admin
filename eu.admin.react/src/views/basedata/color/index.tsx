aeafcae479dbca71607619051bc95f2924d32c8b
xiaochanghai <xiaochanghai@foxmail.com>
2025-06-05 16:28:14 +0800

    perf(视图): 使用React.memo优化颜色管理组件
    将颜色管理视图组件包装为React.memo以提升性能，并添加详细注释说明

--- eu.admin.react/src/views/basedata/color/index.tsx
+++ eu.admin.react/src/views/basedata/color/index.tsx
@@ -1,8 +1,17 @@
-import Main from "../../system/common/main";
 import React from "react";
+import Main from "../../system/common/main";
 
-const Index: React.FC = () => {
+/**
+ * 颜色管理模块
+ *
+ * 该组件使用通用的Main组件加载颜色管理模块信息并渲染对应的表单。
+ * Main组件会处理模块信息的获取和状态管理，并根据模块配置渲染适当的界面。
+ *
+ * 模块代码说明：
+ * - BD_COLOR_MNG: 基础数据-颜色管理模块代码
+ */
+const Index: React.FC = React.memo(() => {
   return <Main moduleCode="BD_COLOR_MNG" />;
-};
+});
 
 export default Index;
