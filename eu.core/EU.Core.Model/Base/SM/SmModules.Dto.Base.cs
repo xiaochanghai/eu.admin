f10919e146b71955c979973c9f86ac888092cd75
xiaochanghai <xiaochanghai@foxmail.com>
2026-06-13 00:24:21 +0800

    feat(模块管理): 新增表格行选择控制字段，更新版权并重构冗余代码
    - 更新文件头的版权年份与版本日期
    - 移除SmModules中已继承自BasePoco的冗余属性
    - 在DTO和实体类中添加IsDisplayRowSelection字段以控制表格行选择

--- eu.core/EU.Core.Model/Base/SM/SmModules.Dto.Base.cs
+++ eu.core/EU.Core.Model/Base/SM/SmModules.Dto.Base.cs
@@ -6,9 +6,9 @@
 *
 * Ver    变更日期 负责人  变更内容
 * ───────────────────────────────────
-* V0.01  2025/6/4 11:12:45  SahHsiao   初版
+* V0.01  2026/6/13 0:21:02  SahHsiao   初版
 *
-* Copyright(c) 2025 EU Corporation. All Rights Reserved.
+* Copyright(c) 2026 EU Corporation. All Rights Reserved.
 *┌──────────────────────────────────┐
 *│　此技术信息为本公司机密信息，未经本公司书面同意禁止向第三方披露．　│
 *│　作者：SahHsiao                                                  │
@@ -202,4 +202,10 @@ public class SmModulesBase : BasePoco
     /// </summary>
     [Display(Name = "IsImportExcel"), Description("是否导入Excel")]
     public bool? IsImportExcel { get; set; }
+
+    /// <summary>
+    /// 表格行是否可选择
+    /// </summary>
+    [Display(Name = "IsDisplayRowSelection"), Description("表格行是否可选择")]
+    public bool? IsDisplayRowSelection { get; set; }
 }
